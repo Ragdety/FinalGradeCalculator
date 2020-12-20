6bf4ad4b26c3db4215074023a6c44e6a8604f947
Edgar Terrazas <Ragdety@users.noreply.github.com>
2020-12-20 00:44:23 -0500

    Implemented ICourseService method stubs

--- FinalGradeCalculator.Web/FinalGradeCalculator.Services/CourseService.cs
+++ FinalGradeCalculator.Web/FinalGradeCalculator.Services/CourseService.cs
@@ -1,9 +1,29 @@
-﻿using System;
+﻿using FinalGradeCalculator.Data.Models;
+using System;
+using System.Collections.Generic;
 
 namespace FinalGradeCalculator.Services
 {
     public class CourseService : ICourseService
     {
+        public void AddCourse(Course course)
+        {
+            throw new NotImplementedException();
+        }
 
+        public void DeleteCourse(int courseId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Course> GetAllCourses()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Course GetCourse(int courseId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
