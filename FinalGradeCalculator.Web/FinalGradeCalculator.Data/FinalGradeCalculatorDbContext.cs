1775700930be48723645464ac09853890a799b3e
Edgar Terrazas <Ragdety@users.noreply.github.com>
2020-12-20 00:41:42 -0500

    Created Db Context for database operations

---
+++ FinalGradeCalculator.Web/FinalGradeCalculator.Data/FinalGradeCalculatorDbContext.cs
@@ -0,0 +1,17 @@
+﻿using FinalGradeCalculator.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinalGradeCalculator.Data
+{
+    public class FinalGradeCalculatorDbContext : DbContext
+    {
+        public FinalGradeCalculatorDbContext() { }
+        public FinalGradeCalculatorDbContext(DbContextOptions options) : base(options) { }
+
+        public virtual DbSet<Course> Courses { get; set; }
+        public virtual DbSet<GradedItem> GradedItems { get; set; }
+    }
+}
