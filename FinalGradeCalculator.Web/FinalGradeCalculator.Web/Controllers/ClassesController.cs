f20fa459ba82e6cfff5c3497e3fed9cd4188243d
Edgar Terrazas <Ragdety@users.noreply.github.com>
2020-12-19 23:01:22 -0500

    Added Class Controller

---
+++ FinalGradeCalculator.Web/FinalGradeCalculator.Web/Controllers/ClassesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace FinalGradeCalculator.Web.Controllers
+{
+    [Route("/api/classes")]
+    [ApiController]
+    public class ClassesController : ControllerBase
+    {
+        private readonly ILogger<ClassesController> _logger;
+
+        public ClassesController(ILogger<ClassesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult GetGrades()
+        {
+            return Ok("Grades!");
+        }
+    }
+}
