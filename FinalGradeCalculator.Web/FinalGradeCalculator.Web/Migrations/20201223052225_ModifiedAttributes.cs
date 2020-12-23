c51bd848b1ce58530aa58c3dfc456f06452c73ff
Edgar Terrazas <Ragdety@users.noreply.github.com>
2020-12-23 00:37:19 -0500

    Deleted Useless files to GitHub

---
+++ FinalGradeCalculator.Web/FinalGradeCalculator.Web/Migrations/20201223052225_ModifiedAttributes.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FinalGradeCalculator.Web.Migrations
+{
+    public partial class ModifiedAttributes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "GradedItems",
+                maxLength: 150,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Courses",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Instructor",
+                table: "Courses",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "GradedItems",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 150);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Courses",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 200);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Instructor",
+                table: "Courses",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 200);
+        }
+    }
+}
