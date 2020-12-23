7389137fcd1ff7f2f196c9b00a6056158ee0f385
Edgar Terrazas <Ragdety@users.noreply.github.com>
2020-12-23 16:03:02 -0500

    Added GradedItem service

---
+++ FinalGradeCalculator.Web/FinalGradeCalculator.Services/GradedItemService.cs
@@ -0,0 +1,31 @@
+﻿using FinalGradeCalculator.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalGradeCalculator.Services
+{
+    public class GradedItemService : IGradedItemService
+    {
+        public Task AddGradedItem(GradedItem gradedItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteGradedItem(int gradedItemId, int courseId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<GradedItem>> GetAllGradedItemsFromCourse(int courseId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GradedItem> GetGradedItemFromCourse(int gradedItemId, int courseId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
