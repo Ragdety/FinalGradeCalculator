61c92b65a68031a64e5d46c35e3988e2a5e4def9
Edgar Terrazas <Ragdety@users.noreply.github.com>
2021-01-02 23:09:55 -0500

    Added Header Component

---
+++ FinalGradeCalculator Client/final-grade-calculator/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className="text-center display-3">Courses</h1>
+        </div>
+    );
+}
+
+export default Header;
