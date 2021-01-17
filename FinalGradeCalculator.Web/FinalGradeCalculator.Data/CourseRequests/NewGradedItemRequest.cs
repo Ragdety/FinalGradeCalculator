b7e5e85bf41946b047f6d626ca6e0dd4d58b3183
Edgar Terrazas <Ragdety@users.noreply.github.com>
2021-01-16 23:35:14 -0500

    Added Course Requests for Data project

---
+++ FinalGradeCalculator.Web/FinalGradeCalculator.Data/CourseRequests/NewGradedItemRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalGradeCalculator.Web.CourseRequests
+{
+    public class NewGradedItemRequest
+    {
+        public string Name { get; set; }
+        public double Grade { get; set; }
+    }
+}
