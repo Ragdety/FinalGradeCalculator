d0391c3254f4c3ef256c63aa2c3642609d445a80
Edgar Terrazas <Ragdety@users.noreply.github.com>
2021-01-18 15:19:34 -0500

    Renamed some files

---
+++ FinalGradeCalculator.Web/FinalGradeCalculator.Data/Requests/CourseRequest.cs
@@ -0,0 +1,15 @@
+﻿using FinalGradeCalculator.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalGradeCalculator.Data.Requests
+{
+    public class CourseRequest
+    {
+        public string Name { get; set; }
+        public string Instructor { get; set; }
+        public ICollection<GradedItemRequest> GradeItems { get; set; }
+    }
+}
---
+++ FinalGradeCalculator.Web/FinalGradeCalculator.Data/Requests/GradedItemRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalGradeCalculator.Data.Requests
+{
+    public class GradedItemRequest
+    {
+        public string Name { get; set; }
+        public double Grade { get; set; }
+    }
+}
