642d5f9126c70fde9f75195b088459cffb638750
Edgar Terrazas <Ragdety@users.noreply.github.com>
2021-01-18 23:23:58 -0500

    Added method to get all graded items from course

--- FinalGradeCalculator.Web/FinalGradeCalculator.Web/Controllers/GradedItemController.cs
+++ FinalGradeCalculator.Web/FinalGradeCalculator.Web/Controllers/GradedItemController.cs
@@ -2,15 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FinalGradeCalculator.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FinalGradeCalculator.Web.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("/api/gradedItems")]
     [ApiController]
     public class GradedItemController : ControllerBase
     {
+        private readonly IGradedItemService _gradedItemService;
 
+        public GradedItemController(IGradedItemService gradedItemService)
+        {
+            _gradedItemService = gradedItemService;
+        }
+
+        [HttpGet("/api/gradedItems/{courseId}")]
+        public async Task<IActionResult> GetAllGradedItemsFromCourse(int courseId)
+        {
+            try
+            {
+                var gradedItems = await _gradedItemService.GetAllGradedItemsFromCourse(courseId);
+
+                if (gradedItems == null)
+                    return NotFound($"No graded items found for course with id: {courseId}");
+
+                return Ok(gradedItems);
+            }
+            catch(InvalidOperationException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
     }
 }
