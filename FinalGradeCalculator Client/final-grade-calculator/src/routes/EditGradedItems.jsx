861a80c46bece82f9af03156394c13c47d927f47
Edgar Terrazas <Ragdety@users.noreply.github.com>
2021-01-24 01:22:36 -0500

    Added necessary components

--- FinalGradeCalculator Client/final-grade-calculator/src/routes/EditGradedItems.jsx
+++ FinalGradeCalculator Client/final-grade-calculator/src/routes/EditGradedItems.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import GradedItems from '../components/GradedItem/GradedItems';
+import Header from '../components/GradedItem/Header';
+import AddGradedItem from '../components/GradedItem/AddGradedItem';
 
 const EditGradedItems = () => {
     return (
         <div className="container">
-            
+            <Header/>
+            <AddGradedItem/>
             <GradedItems/>
         </div>
     );
